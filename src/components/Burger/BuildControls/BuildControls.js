635c5bf71026f955469c183e4c187d7d859eb9cd
michael.lau <michael.lau@sinodynamic.com>
2018-07-16 14:15:57 +0800

    Add and Remove function for the Burger Builder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import classes from './../BuildControls/BuildControls.css';
-
-
-
 import BuildControl from './BuildControl/BuildControl';
+
 
 const controls = [
   {label: 'Salad', type:'salad'},
@@ -20,7 +17,8 @@ const buildControls = (props) =>(
                 key={ctrl.label} 
                 label={ctrl.label}
                 type={ctrl.type}
-                added={()=>props.ingredientAdded(ctrl.type)}  />
+                added={()=>props.ingredientAdded(ctrl.type)}  
+                removed={()=> props.ingredientRemoved(ctrl.type)}/>
         ))}
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,10 +23,25 @@ class BurgerBuilder extends Component{
     }
 
 
+    removeIngredientHandler =(type) =>{
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return ;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]=updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+    }
 
     addIngredientHandler =(type) =>{
         const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount +1;
+        const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
@@ -37,16 +52,15 @@ class BurgerBuilder extends Component{
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
     }
 
-    removeIngredientHandler = (type) =>{
-
-    }
-
+   
     render(){
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
-                   ingredientAdded={this.addIngredientHandler}/>
+                   ingredientAdded={this.addIngredientHandler}
+                   ingredientRemoved={this.removeIngredientHandler}
+                   />
             </Aux>
         );
     }
